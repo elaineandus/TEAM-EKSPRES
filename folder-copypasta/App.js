04c351b159e8a76071d1f4992086eb8816946448
elaineandus <91137692+elaineandus@users.noreply.github.com>
2022-12-22 10:17:20 +0800

    added js files

---
+++ folder-copypasta/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>ITO NA ANG BAGO! HERE TAYO MAG EEDIT MGA ANTWEH!</Text>
+      <Text>ANTWEH TWEH TWEH TWEH FRAGILE FRAGILE</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
